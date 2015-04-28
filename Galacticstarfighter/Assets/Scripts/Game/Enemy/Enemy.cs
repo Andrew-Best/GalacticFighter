43be7644e462c230c613cc89ec4b83f7ff692b4c
JimCorrigan <cyginus@gmail.com>
2015-04-28 12:34:21 -0400

    Unity game upload

---
+++ Galacticstarfighter/Assets/Scripts/Game/Enemy/Enemy.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Enemy : MonoBehaviour
+{
+    public Ship m_Ship;
+    public ShipController m_ShipController;
+    public ShipData m_ShipData;
+    public GameObject m_Target;
+
+    public abstract void Awake();
+    public abstract void OnExit();
+    public abstract void Update();
+    public abstract void SetProjectiles();
+    
+}
---
+++ Galacticstarfighter/Assets/Scripts/Game/Ship/EngineData.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EngineData : MonoBehaviour 
+{
+
+    public float m_ForwardAccel;
+    public float m_FuelConsumption;
+    public float m_VerticalAccel;
+
+    public float m_CargoCap;
+}
